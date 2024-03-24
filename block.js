8cd7bb6bc7b43cdb5e53756ada914aa93cd91309
Khilee01 <khileesinghal@gmail.com>
2024-03-24 18:36:11 +0530

    lec S_2_8 to S_2_16

---
+++ block.js
@@ -0,0 +1,25 @@
+const {GENESIS_DATA}=require("./config");
+const cryptoHash = require("./crypto-hash");
+class Block{
+   constructor({timestamp,lastHash,hash,data}){
+        this.timestamp=timestamp;
+        this.data=data;
+        this.hash=hash;
+        this.lastHash=lastHash;
+   }
+   static genesis(){
+      return new this(GENESIS_DATA);
+   }
+   
+   static mineBlock({lastBlock,data}){
+      const timestamp=Date.now();
+      const lastHash=lastBlock.hash
+      return new this({
+         timestamp,
+         lastHash,
+         data,
+         hash:cryptoHash(timestamp,lastHash,data)
+      });
+   }
+}
+module.exports = Block;
